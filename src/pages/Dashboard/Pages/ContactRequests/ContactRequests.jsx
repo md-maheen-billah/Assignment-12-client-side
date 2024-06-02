a8277ff0b4696aa56e6eedea8dadc248bf40ec73
md-maheen-billah <md.maheen.billah@icloud.com>
2024-06-02 22:59:09 +0200

    approving contact request completed

--- src/pages/Dashboard/Pages/ContactRequests/ContactRequests.jsx
+++ src/pages/Dashboard/Pages/ContactRequests/ContactRequests.jsx
@@ -1,7 +1,64 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+
 const ContactRequests = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: requests = [], refetch } = useQuery({
+    queryKey: ["requests"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/requested-access-dashboard`);
+      return data;
+    },
+  });
+  console.log(requests);
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ sdata, id }) => {
+      const { data } = await axiosSecure.patch(
+        `/requested-access-dashboard/${id}`,
+        sdata
+      );
+      return data;
+    },
+    onSuccess: () => {
+      refetch();
+      toast.success("Approved!");
+    },
+  });
+
+  const handleChangeStatus = async (id) => {
+    const status = "approved";
+
+    try {
+      const sdata = {
+        status,
+      };
+      console.table(sdata);
+
+      //   Post request to server
+      await mutateAsync({ id, sdata });
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
   return (
     <div>
       <h2>Contact Requests</h2>
+      {requests.map((item) => (
+        <div key={item._id}>
+          <p>Requested Person: {item.name}</p>
+          <p>Requested Id: {item.biodataId}</p>
+          <p>Requested By: {item.email}</p>
+          <button
+            onClick={() => handleChangeStatus(item._id)}
+            className="px-4 py-2 bg-slate-700 text-white"
+          >
+            {item.status}
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
